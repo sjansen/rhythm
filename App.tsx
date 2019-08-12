a30b23cbf5acb7973081810f4332954b18c76504
Stuart Jansen <sjansen@buscaluz.org>
2019-08-12 07:18:31 -0600

    integrate react-native-keychain

--- App.tsx
+++ App.tsx
@@ -1,7 +1,17 @@
 import React, { Fragment } from 'react';
-import { SafeAreaView, StyleSheet, View, Text, StatusBar } from 'react-native';
+import * as Keychain from 'react-native-keychain';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  Button,
+} from 'react-native';
 
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const SERVER = 'example.com';
 
 const styles = StyleSheet.create({
   body: {
@@ -17,12 +27,46 @@ const styles = StyleSheet.create({
 });
 
 const App = () => {
+  const getCredentials = async (): Promise<Keychain.UserCredentials> => {
+    const tmp = await Keychain.getInternetCredentials(SERVER);
+    return tmp;
+  };
+
+  const setCredentials = async (username: string, password: string) => {
+    await Keychain.setInternetCredentials(SERVER, username, password, {
+      accessControl: Keychain.ACCESS_CONTROL.USER_PRESENCE,
+      accessible: Keychain.ACCESSIBLE.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
+    });
+  };
+
+  const handleCheckCredentials = async () => {
+    const credentials = await getCredentials();
+    if (credentials) {
+      console.log(credentials);
+      try {
+        await Keychain.resetInternetCredentials(SERVER);
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      console.log('No credentials stored');
+      try {
+        await setCredentials('foo', 'bar');
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <View style={styles.body}>
           <Text style={styles.sectionTitle}>This Space For Rent</Text>
+        </View>
+        <View style={styles.body}>
+          <Button onPress={handleCheckCredentials} title="Check Credentials" />
         </View>
       </SafeAreaView>
     </Fragment>

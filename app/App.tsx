237abda8ff88d103c96bbf580f22fbf6086127ef
Stuart Jansen <sjansen@buscaluz.org>
2019-12-13 22:06:30 -0700

    restart mobile app

--- app/App.tsx
+++ app/App.tsx
@@ -1,76 +1,19 @@
-import React, { Fragment } from 'react';
-import * as Keychain from 'react-native-keychain';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-  StatusBar,
-  Button,
-} from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-
-const SERVER = 'example.com';
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionTitle: {
-    color: Colors.black,
-    fontSize: 24,
-    fontWeight: '600',
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-const App = () => {
-  const getCredentials = async (): Promise<Keychain.UserCredentials> => {
-    const tmp = await Keychain.getInternetCredentials(SERVER);
-    return tmp;
-  };
-
-  const setCredentials = async (username: string, password: string) => {
-    await Keychain.setInternetCredentials(SERVER, username, password, {
-      accessControl: Keychain.ACCESS_CONTROL.USER_PRESENCE,
-      accessible: Keychain.ACCESSIBLE.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
-    });
-  };
-
-  const handleCheckCredentials = async () => {
-    const credentials = await getCredentials();
-    if (credentials) {
-      console.log(credentials);
-      try {
-        await Keychain.resetInternetCredentials(SERVER);
-      } catch (e) {
-        console.log(e);
-      }
-    } else {
-      console.log('No credentials stored');
-      try {
-        await setCredentials('foo', 'bar');
-      } catch (e) {
-        console.log(e);
-      }
-    }
-  };
-
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View style={styles.body}>
-          <Text style={styles.sectionTitle}>This Space For Rent</Text>
-        </View>
-        <View style={styles.body}>
-          <Button onPress={handleCheckCredentials} title="Check Credentials" />
-        </View>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
-
-export default App;
